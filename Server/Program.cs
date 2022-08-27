bf6b07f07194f298fff202596fe99586d9ef15be
OliverSchlueter <oliverhd1303@gmail.com>
2022-08-27 17:34:18 +0200

    Added error to test workflow

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,7 +4,7 @@ public static class Program
 {
     private static void Main()
     {
-        Console.Title = "CallcenterSimulation | Server";
+        Console.Title = "CallcenterSimulation | Server"
         
         Console.WriteLine($"Starting server application now (v{ServerMain.Version})");
         ServerMain main = ServerMain.CreateOrGetInstance();
