cfdcbc5505f3a8304815fc36899ecb3894f920b4
Oliver <79666085+OliverSchlueter@users.noreply.github.com>
2022-08-29 11:47:20 +0200

    Release 2.0.0

--- Server/ServerMain.cs
+++ Server/ServerMain.cs
@@ -11,7 +11,7 @@ public class ServerMain
     private static ServerMain _instance;
     public static ServerMain Instance => _instance;
 
-    public const string Version = "1.1.2-rc.2";
+    public const string Version = "2.0.0";
 
     private const int AutoRestartDelay = 60*6; // in minutes
 
